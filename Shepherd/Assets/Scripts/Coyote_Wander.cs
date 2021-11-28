9b86b130cfd406339fa7b82f03cb91762a416769
JoshuaHecht <89319822+JoshuaHecht@users.noreply.github.com>
2021-11-28 14:43:15 -0500

    Coyote on bridge
    coyote on bridge with new path

--- Shepherd/Assets/Scripts/Coyote_Wander.cs
+++ Shepherd/Assets/Scripts/Coyote_Wander.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Coyote_Wander : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Transform[] waypoints;
+    public NavMeshAgent nav;
+    int current = 0;
+    int phase = 1;
     // Update is called once per frame
     void Update()
     {
-        
+        switch (phase)
+        {
+            case 1:
+                // PHASE 1: Coyote on bridge
+                Vector2 currentPos = new Vector2(transform.position.x, transform.position.z);
+                Vector2 destination = new Vector2(waypoints[current].position.x, waypoints[current].position.z);
+                float distance = Vector2.Distance(currentPos, destination);
+                if (distance > 2)
+                {
+                    nav.SetDestination(waypoints[current].position);
+                    Debug.Log("not met");
+                }
+                else
+                {
+                    if (current == 4) { current = 1; }
+                    else current += 1;
+                    Debug.Log("destination met");
+                }
+                break;
+        }
+    }
+
+    void SetPhase(int phase)
+    {
+        //placeholder
     }
 }
