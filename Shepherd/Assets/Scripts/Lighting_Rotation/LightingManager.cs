bfc65736d22574e688f05f631c43530f846b281a
gbgootee <gbgootee@iu.edu>
2021-12-05 04:55:50 -0500

    Added Day/Night cycle Complete

--- Shepherd/Assets/Scripts/Lighting_Rotation/LightingManager.cs
+++ Shepherd/Assets/Scripts/Lighting_Rotation/LightingManager.cs
@@ -2,17 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
+
 public class LightingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Scene References
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingPreset Preset;
+    //Variables
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+    [SerializeField, Range(-10, 10)] private float speedMultiplier;  // used to adjust the cycle time. Note that values < 0 will reverse it!
+    [SerializeField, Range(1, 10)] private float nightSpeed; // how much to speed up late-night hours
+    [SerializeField] private float maxIntensity = 1.5f;
+    private float baseIntensity = 0f;
+    [SerializeField] private float maxShadowStrength = 1f;
+    [SerializeField] private float minShadowStrength = 0.2f;
+    private float nightSpeedUpStart = 20f;
+    private float nightSpeedUpEnd = 4f;
+    private float dawn = 20f;
+    private float dusk = 20f;
+    private float noon = 20f;
+
+    private void UpdateLighting(float timePercent)
     {
-        
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+
+        if(DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170, 0));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
-        
+        //default values
+        speedMultiplier = 0.08f;
+        nightSpeed = 10.0f;
+        baseIntensity = maxIntensity / 2f;
+    }
+
+    private void Update()
+    {
+        if (Preset == null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            //(Replace with a reference to the game time)
+            //speed up the time in dead of night
+            if (TimeOfDay > nightSpeedUpStart || TimeOfDay < nightSpeedUpEnd) //speed up the passage of night from 9pm to 3am
+            {
+                //TimeOfDay += Time.deltaTime * speedMultiplier * nightSpeed;
+                TimeOfDay += Time.deltaTime * nightSpeed;
+            }
+            else
+            {
+                TimeOfDay += Time.deltaTime * speedMultiplier;
+
+                //adjust light intensity and shadow softness for time of day
+                if (TimeOfDay >= dawn && TimeOfDay <= noon)
+                {
+                    DirectionalLight.intensity = baseIntensity + (baseIntensity / (noon - dawn)) * (TimeOfDay - dawn);
+                    DirectionalLight.shadowStrength = minShadowStrength + ((maxShadowStrength - minShadowStrength) / (noon - dawn)) * (TimeOfDay - dawn);
+                }
+                else if (TimeOfDay > noon && TimeOfDay <= dusk)
+                {
+                    DirectionalLight.intensity = baseIntensity + (baseIntensity / (dusk - noon)) * (dusk - TimeOfDay);
+                    DirectionalLight.shadowStrength = minShadowStrength + ((maxShadowStrength - minShadowStrength) / (dusk - noon)) * (dusk - TimeOfDay);
+                }
+                else
+                {
+                    DirectionalLight.intensity = baseIntensity;
+                    DirectionalLight.shadowStrength = minShadowStrength;
+
+                }
+            }
+            TimeOfDay %= 24; //always between 0-24
+            UpdateLighting(TimeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
     }
 }
